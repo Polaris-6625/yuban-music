26b9320b6a91db15010059c63f67213efa8d47f4
liuyuyang <2281487673@qq.com>
2023-10-03 08:37:30 +0800

    add(service.js)增加了对于ios的兼容适配

--- service.js
+++ service.js
@@ -1,10 +1,12 @@
 // service.js
+import { Platform } from 'react-native';
 import TrackPlayer , { Event } from 'react-native-track-player';
 
 module.exports = async function() {
     // This service needs to be registered for the module to work
     // but it will be used later in the "Receiving Events" section
-    TrackPlayer.addEventListener(Event.RemotePause, () => {
+    if (Platform.OS === 'android') {
+      TrackPlayer.addEventListener(Event.RemotePause, () => {
         console.log('Event.RemotePause');
         TrackPlayer.pause();
       });
@@ -86,4 +88,28 @@ module.exports = async function() {
           });
         }
       );
+    }
+    else {
+      addEventListener('remote-play', () => TrackPlayer.play());
+
+      addEventListener('remote-pause', () => TrackPlayer.pause());
+
+      addEventListener('remote-stop', () => TrackPlayer.destroy());
+
+      addEventListener('remote-next', () => TrackPlayer.skipToNext());
+
+      addEventListener('remote-previous', async (param) => skipToPrevious());
+
+      addEventListener('remote-jump-forward', async ({interval}) => {
+        const position = await TrackPlayer.getPosition();
+        TrackPlayer.seekTo(position + interval);
+      });
+
+      addEventListener('remote-jump-backward', async ({interval}) => {
+        const position = await TrackPlayer.getPosition();
+        TrackPlayer.seekTo(position - interval);
+      });
+      
+      addEventListener('remote-seek', ({position}) => TrackPlayer.seekTo(position));
+    }
 }
